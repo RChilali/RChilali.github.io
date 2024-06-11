50cc049c3b0c6ca7907b490565e0075c5baafcc1
Rayane Chilali <chilali.lmc@gmail.com>
2024-06-11 14:45:48 +0200

    add video from anonym source

--- js/languageSwitch.js
+++ js/languageSwitch.js
@@ -1,40 +1,10 @@
-const translations = {
-  en: {
-    welcome: "Hi, I'm Rayane Chilali",
-    role: "Web Developer",
-    location: "Based in Paris",
-    specialization: "Specialized in Back-End Development",
-    skills: "Java , Spring, Vue.js",
-    availability: "Looking for new opportunities",
-    flag: "img/gb.svg"
-  },
-  fr: {
-    welcome: "Bonjour, je suis Rayane Chilali",
-    role: "Développeur Web",
-    location: "Basé à Paris",
-    specialization: "Spécialisé dans le développement Back-End",
-    skills: "Java , Spring, Vue.js",
-    availability: "À la recherche de nouvelles opportunités",
-    flag: "img/fr.svg"
-  },
-  bg: {
-    welcome: "Здравейте, аз съм Раян Шилали",
-    role: "Уеб разработчик",
-    location: "Базиран в Париж",
-    specialization: "Специализиран в Back-End разработката",
-    skills: "Java , Spring, Vue.js",
-    availability: "Търся нови възможности",
-    flag: "img/bg.svg"
-  }
-};
-
 const languageSwitch = document.getElementById('languageSwitch');
 const selectedLanguage = document.getElementById('selectedLanguage');
 
 languageSwitch.addEventListener('click', function(e) {
   e.preventDefault();
   const language = e.target.dataset.value;
-  const flagSrc = translations[language].flag;
+  const flagSrc = e.target.dataset.src;
   localStorage.setItem('language', language); // Store the selected language in localStorage
   localStorage.setItem('flag', flagSrc); // Store the flag source in localStorage
   changeLanguage(language);
@@ -42,13 +12,16 @@ languageSwitch.addEventListener('click', function(e) {
 });
 
 // Function to change the language
-function changeLanguage(language) {
-  document.getElementById("welcome").textContent = translations[language].welcome;
-  document.getElementById("role").textContent = translations[language].role;
-  document.getElementById("location").textContent = translations[language].location;
-  document.getElementById("specialization").textContent = translations[language].specialization;
-  document.getElementById("skills").textContent = translations[language].skills;
-  document.getElementById("availability").textContent = translations[language].availability;
+async function changeLanguage(language) {
+  const response = await fetch(`data/${language}.json`);
+  const translations = await response.json();
+
+  document.getElementById("welcome").textContent = translations.welcome;
+  document.getElementById("role").textContent = translations.role;
+  document.getElementById("location").textContent = translations.location;
+  document.getElementById("specialization").textContent = translations.specialization;
+  document.getElementById("skills").textContent = translations.skills;
+  document.getElementById("availability").textContent = translations.availability;
 }
 
 // When the page loads, check if a language has been stored in localStorage
